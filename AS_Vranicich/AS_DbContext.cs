f05ec4148370d6925825011036cce54485c87789
Alex Vranicich <alex.vranicich@gmail.com>
2023-01-05 22:06:11 +0100

    MyFirstCommit

---
+++ AS_Vranicich/AS_DbContext.cs
@@ -0,0 +1,13 @@
+﻿using AS_Vranicich.AS_Models;
+using Microsoft.EntityFrameworkCore;
+using TAP22_23.AuctionSite.Interface;
+
+namespace AS_Vranicich
+{
+    public class AS_DbContext : TapDbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+
+    }
+}
---
+++ AS_Vranicich/AS_Models/Session.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS_Vranicich.AS_Models
+{
+    public class Session
+    {
+        public int SessionId { get; }
+        public DateTime ValidUntil { get; }
+
+        public User Owner { get; }
+        public int UserId { get; }
+    }
+}
